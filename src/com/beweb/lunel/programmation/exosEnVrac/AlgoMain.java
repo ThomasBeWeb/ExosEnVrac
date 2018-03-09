d20d111c99fc24d6b6f93fb8c1facfb906314ac2
cantinelli <cantinelli@beweb-16.176C>
2018-03-09 19:08:57 +0100

    init

---
+++ src/com/beweb/lunel/programmation/exosEnVrac/AlgoMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beweb.lunel.programmation.exosEnVrac;
+
+/**
+ *
+ * @author cantinelli
+ */
+public class AlgoMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Exercice1.launch(); // execution de l'exercice 1
+
+
+    }
+    
+}
